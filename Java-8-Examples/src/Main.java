57c7a0f095b4ba06a51a35aebbb1831c48d58d9f
neerajpatel-489 <patelneeraj489@gmail.com>
2021-01-24 18:56:20 +0530

    lambda exp

--- Java-8-Examples/src/Main.java
+++ Java-8-Examples/src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args){
         System.out.println("HELLO JAVA 8");
 
+        //lambda without return statement
+        System.out.println("Lambda with return statement and {}");
         Sayable sayable = ()->{
             return "Hello Java Function";
         };
@@ -14,8 +16,18 @@ public class Main {
         SayName sayName = (name)->{
             return "HELLO "+name;
         };
-
         System.out.println("Say Name out =>"+sayName.sayName("NEERAJ"));
+        System.out.println("============================================================");
+        //lambda without return statement
+        System.out.println("Lambda without return statement and {}");
+
+        Sayable sayable1 = ()-> "Hello Java Function";
+
+        System.out.println("Say able output =>"+ sayable1.say());
+
+        SayName sayName1 = (name)->"HELLO "+name;
+
+        System.out.println("Say Name out =>"+sayName1.sayName("NEERAJ"));
 
     }
 }
